0765cd3d07f0864768c7c51f708f8e7ab05aa67b
gudgle <108617925+gudgle@users.noreply.github.com>
2022-07-03 20:21:46 +0200

    Update travel.tsx

--- src/components/travel.tsx
+++ src/components/travel.tsx
@@ -9,23 +9,23 @@ const Travel = (): JSX.Element => {
       </h1>
       <Text value={'travel_from_trondheim'} />
       <h2>
-        <Text value={'travel_from_trondheim_2'} />
+        <Text value={'boat'} />
       </h2>
       <p>
-        <Text value={'travel_from_trondheim_3'} />
-      </p>
-      <h2>
-        <Text value={'travel_from_trondheim_4'} />
-      </h2>   
-      <p>
-        <Text value={'travel_from_trondheim_5'} />
+        <Text value={'boat_desc'} />
         <a
         href="https://www.atb.no/hitra/"
         target="_blank"
         >
-        <Text value={'travel_from_trondheim_5_link'} />
+        <Text value={'boat_desc_link'} />
         </a>
-        <Text value={'travel_from_trondheim_6'} />
+      </p>
+      
+      <h2>
+        <Text value={'car'} />
+      </h2>   
+      <p>
+        <Text value={'car_desc'} />
       </p>
       </h2>
     </div>
